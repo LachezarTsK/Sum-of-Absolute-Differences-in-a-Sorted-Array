83d995f898ce70f8e81995288d3189fe0bae7c64
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-12-03 14:40:01 +0100

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,32 @@
+
+public class Solution {
+
+    public int[] getSumAbsoluteDifferences(int[] input) {
+        final int totalElements = input.length;
+
+        //alternatively:  Arrays.stream(input).sum()
+        int sumAllElements = 0;
+        for (int value : input) {
+            sumAllElements += value;
+        }
+
+        int[] sumAbsoluteDifferences = new int[totalElements];
+        int sumPreceedingElements = 0;
+
+        for (int i = 0; i < totalElements; ++i) {
+            int numberOfPreceedingElements = i;
+            int numberOfFollowingElements = totalElements - 1 - i;
+
+            int sumFollowingElements = sumAllElements - sumPreceedingElements - input[i];
+
+            int preceedingValuesDifferences = (input[i] * numberOfPreceedingElements) - sumPreceedingElements;
+            int followingValuesDifferences = sumFollowingElements - (input[i] * numberOfFollowingElements);
+
+            sumAbsoluteDifferences[i] = preceedingValuesDifferences + followingValuesDifferences;
+
+            sumPreceedingElements += input[i];
+        }
+
+        return sumAbsoluteDifferences;
+    }
+}
